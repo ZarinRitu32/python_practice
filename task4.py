cedceec367c168d860207917491f99f1dec4349c
ZarinTasnimRitu <tasnimritu906@gmail.com>
2025-06-27 11:25:02 +0600

    4

---
+++ task4.py
@@ -0,0 +1,19 @@
+'''
+n =1
+for i in range(3):
+  for j in range(3):
+    print(n, end = " ")
+    n += 1
+  print ()  
+  '''
+number = 1
+i = 0
+while i<3:
+    j = 0
+    while j<3:
+        print(number, end = " ")
+        number += 1
+        j += 1
+    print ()
+    i += 1
+
