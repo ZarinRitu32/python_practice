ce0583d02230b7b7138135fc65b4d2c3fab503fb
ZarinTasnimRitu <tasnimritu906@gmail.com>
2025-06-28 21:44:49 +0600

    f

---
+++ function_task.py
@@ -0,0 +1,12 @@
+def NID(name, father_name , mother_name, present_address, permanent_address, gender, dob, blood_group):
+    print(f"Name:{name}")
+    print(f"Father's name: {father_name}")
+    print(f"Mother's name: {mother_name}")
+    print(f"Present Address: {present_address}")
+    print(f"Permanent Address: {permanent_address}")
+    print(f"Gender: {gender}")
+    print(f"Date of Birth: {dob}")
+    print(f"Blood Group: {blood_group}")
+
+
+NID("Zarin Tasnim Ritu","MD. Rafiqul Islam","Mst. Sharifa Akter","Dhaka Cantonment" , "Rangpur","Female","03/12/2003","A-")
